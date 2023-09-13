5cb7f87a1c9f50c410ea21441f944d31b5668fbc
NomadWithoutAHome <96033888+NomadWithoutAHome@users.noreply.github.com>
2023-09-13 12:50:32 -0400

    ffff
    ff

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import json
 import uuid
-
 import requests
 from flask import Flask, render_template, request, redirect, url_for
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 # Load episode data from the JSON file
 with open('episode_data.json', 'r') as file:
@@ -13,28 +12,29 @@ with open('episode_data.json', 'r') as file:
 # Define a dictionary to store UUIDs associated with episodes
 episode_uuids = {}
 
+# Function to generate and store UUIDs for episodes
+def generate_episode_uuids():
+    for season_name, season_episodes in episode_data.items():
+        for episode in season_episodes:
+            episode_uuid = str(uuid.uuid4())
+            episode_uuids[episode_uuid] = episode
+            episode['uuid'] = episode_uuid
 
+# Initialize UUIDs when the application starts
+generate_episode_uuids()
+
+# Function to get season data by season number
 def get_season_data(season_number):
     season_name = f"Season {season_number}"
-    if season_name in episode_data:
-        # Generate a UUID for each video and add it to the data
-        for episode in episode_data[season_name]:
-            episode_uuid = str(uuid.uuid4())
-            episode_uuids[episode_uuid] = episode  # Store the episode data with the UUID as the key
-            episode['uuid'] = episode_uuid  # Add the UUID to the episode data
-        return episode_data[season_name]
-    else:
-        return None  # Season not found
+    return episode_data.get(season_name, None)
 
-
+# Function to get season number by episode title
 def get_season_number(episode_title):
-    for season_name, season_data in episode_data.items():
-        for episode in season_data:
+    for season_name, season_episodes in episode_data.items():
+        for episode in season_episodes:
             if episode['Episode Title'] == episode_title:
-                return season_name.split()[1]
+                return int(season_name.split()[1])
     return None
-
-
 
 # Function to fetch a random quote from The Simpsons API
 def get_random_simpsons_quote():
@@ -45,30 +45,20 @@ def get_random_simpsons_quote():
     else:
         return "Failed to fetch quote", "Unknown"
 
-
 # Define the index page
 @app.route('/')
 def index():
     quote, character, image = get_random_simpsons_quote()
     return render_template('index.html', quote=quote, character=character, image=image)
 
-
+# Define the video page
 @app.route('/video/<uuid:video_uuid>')
 def video(video_uuid):
-    print(episode_uuids)
-    # Convert the UUID to a string
     video_uuid_str = str(video_uuid)
-
-    # Check if the UUID exists in the episode_uuids mapping
-    if video_uuid_str in episode_uuids:
-        episode = episode_uuids[video_uuid_str]
-        video_url = episode.get('Episode_vidsrc')
-        if video_url:
-            return render_template('video.html', video_url=video_url)
-
+    episode = episode_uuids.get(video_uuid_str)
+    if episode and 'Episode_vidsrc' in episode:
+        return render_template('video.html', video_url=episode['Episode_vidsrc'])
     return "Video not found", 404
-
-
 
 # Define the season page
 @app.route('/season/<int:season_number>')
@@ -79,24 +69,24 @@ def season(season_number):
     else:
         return "Season not found", 404
 
-
 # Define the search route
 @app.route('/search')
 def search():
     query = request.args.get('query')
     if query:
-        # Filter the episode data by title
-        search_results = [episode for season in episode_data.values() for episode in season if query.lower() in episode['Episode Title'].lower()]
+        query_lower = query.lower()
+        search_results = []
 
-        # Add the season number to each search result
-        for episode in search_results:
-            episode['Season'] = get_season_number(episode['Episode Title'])
-            episode['Episode Number'] = f"{episode['Season']}x{episode['Episode Number']}"
+        for season_name, season_episodes in episode_data.items():
+            for episode in season_episodes:
+                if query_lower in episode['Episode Title'].lower():
+                    episode_copy = episode.copy()
+                    episode_copy['Season'] = int(season_name.split()[1])
+                    search_results.append(episode_copy)
 
         return render_template('search_results.html', query=query, results=search_results)
     else:
         return redirect(url_for('index'))
-
 
 if __name__ == '__main__':
     app.run(port=5000)
