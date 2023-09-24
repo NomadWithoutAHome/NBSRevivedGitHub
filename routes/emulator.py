5f14cebdc3c43d54becf46628cbda2ddba7b71a2
NomadWithoutAHome <96033888+NomadWithoutAHome@users.noreply.github.com>
2023-09-24 14:37:34 -0400

    fdgdfg
    dfgdf

--- routes/emulator.py
+++ routes/emulator.py
@@ -1,25 +1,22 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
-from pydantic import BaseModel
 
 from main import templates
 
-class Game(BaseModel):
-    game: str
-    emucode: str
-    rom: str
-
 def init_app(app: FastAPI):
     @app.post('/emulator', response_class=HTMLResponse)
-    def emulator(request: Request, game: Game):
+    def emulator(request: Request, game: str = Form(...), emucode: str = Form(...), rom: str = Form(...)):
         """
         Render the emulator.html template with the game data.
 
         Args:
             request (Request): The FastAPI request object.
-            game (Game): The game data.
+            game (str): The game name.
+            emucode (str): The emulator code.
+            rom (str): The ROM file location.
 
         Returns:
             HTMLResponse: The rendered HTML template.
         """
-        return templates.TemplateResponse('emulator.html', {"request": request, "game": game})
+        return templates.TemplateResponse('emulator.html',
+                                          {"request": request, "game": game, "emucode": emucode, "rom": rom})
