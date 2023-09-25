c388405af5bd881db07b4aba67546d1ae355b8bb
NomadWithoutAHome <96033888+NomadWithoutAHome@users.noreply.github.com>
2023-09-25 16:01:31 -0400

    dfd
    dfdf

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from starlette.middleware.base import BaseHTTPMiddleware
 from starlette.responses import FileResponse
 from starlette.types import ASGIApp
+from starlette.exceptions import HTTPException as StarletteHTTPException
 
 from helpers import load_json_data
 app = FastAPI(docs_url=None, redoc_url=None)
@@ -55,16 +56,40 @@ app.add_middleware(
     allow_headers=["*"],  # Allows all headers
 )
 
+
+template_mapping = {
+    400: "400.html",  # Bad Request
+    401: "401.html",  # Unauthorized
+    403: "403.html",  # Forbidden
+    404: "404.html",  # Not Found
+    500: "500.html",  # Internal Server Error
+}
+
+
+
+
+@app.exception_handler(StarletteHTTPException)
+async def http_exception_handler(request, exc):
+    # Get the appropriate template name for the given status code, or use '500.html' as a default
+    template_name = template_mapping.get(exc.status_code, "500.html")
+
+    # Create a TemplateResponse to render the HTML template
+    response = templates.TemplateResponse(template_name, {"request": request}, status_code=exc.status_code)
+
+    # Return the HTML response
+    return response
+
+
 @app.get("/robots.txt")
 async def get_robots_txt():
     # Path to your robots.txt file
     robots_txt_path = "static/robots.txt"
     return FileResponse(robots_txt_path, media_type="text/plain")
 
-@app.get("/sitemap")
+@app.get("/sitemap.xml")
 async def get_sitemap_xml():
-    robots_txt_path = "static/sitemap.xml"
-    return FileResponse(robots_txt_path, media_type="text/xml")
+    sitemap_patch = "static/sitemap.xml"
+    return FileResponse(sitemap_patch, media_type="text/xml")
 
 def get_episode_data():
     return load_json_data('static/data/episode_data.json')
